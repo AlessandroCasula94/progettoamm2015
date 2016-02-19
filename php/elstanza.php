1e7edc380f99ed3646ccbf51e74c3c82a26dadd0
AlessandroCasula94 <luckitte@gmail.com>
2016-02-19 22:55:58 +0100

    Update elstanza.php

--- php/elstanza.php
+++ php/elstanza.php
@@ -21,7 +21,7 @@ session_start();
 					$Connessione=mysql_connect("localhost","casulaAlessandro","dromedario8135");
 					$DataBase=mysql_select_DB("amm15_casulaAlessandro");
 					$codice=$_GET['codi'];
-					$Query="DELETE * FROM Stanza WHERE cod='$codice' ;";
+					$Query="DELETE FROM Stanza WHERE cod='$codice' ;";
 					$ExQuery=mysql_query($Query);
 					mysql_close($Connessione);
 					echo"<div id='testo'>Eliminazione completata correttamente</div>";
