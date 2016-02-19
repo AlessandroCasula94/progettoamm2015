7e23e7426af3111f0562e9c08345091fc79a8290
AlessandroCasula94 <luckitte@gmail.com>
2016-02-19 22:58:00 +0100

    Update elstanza.php

--- php/elstanza.php
+++ php/elstanza.php
@@ -21,6 +21,8 @@ session_start();
 					$Connessione=mysql_connect("localhost","casulaAlessandro","dromedario8135");
 					$DataBase=mysql_select_DB("amm15_casulaAlessandro");
 					$codice=$_GET['codi'];
+					$Query="DELETE FROM Prenotazioni WHERE cod='$codice' ;";
+					$ExQuery=mysql_query($Query);
 					$Query="DELETE FROM Stanza WHERE cod='$codice' ;";
 					$ExQuery=mysql_query($Query);
 					mysql_close($Connessione);
